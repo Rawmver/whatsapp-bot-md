17bb8cfd106c056644218d624a4a9437d2c76926
Rawmver <139946633+Rawmver@users.noreply.github.com>
2023-08-02 09:33:44 +0500

    Update config.js

--- config.js
+++ config.js
@@ -34,7 +34,7 @@ module.exports = {
 	HEROKU_APP_NAME: process.env.HEROKU_APP_NAME,
 	HEROKU_API_KEY: process.env.HEROKU_API_KEY,
 	BRANCH: 'master',
-	STICKER_PACKNAME: process.env.STICKER_PACKNAME || '❤️,LyFE',
+	STICKER_PACKNAME: process.env.STICKER_PACKNAME || '❤️,LuciFER',
 	ALWAYS_ONLINE: toBool(process.env.ALWAYS_ONLINE),
 	LOG_MSG: toBool(process.env.LOG_MSG) || false,
 	RMBG_KEY: process.env.RMBG_KEY || 'null',
@@ -50,7 +50,7 @@ module.exports = {
 		process.env.ANTISPAM_MSG || '_Antispam Detected &mention kicked_',
 	ANTIWORDS_MSG:
 		process.env.ANTIWORDS_MSG || '_AntiWord Detected &mention kicked_',
-	ANTIWORDS: process.env.ANTIWORDS || 'word',
+	ANTIWORDS: process.env.ANTIWORDS || 'fuck,pussy,sex,cunt,vagina',
 	MENTION: process.env.MENTION || '',
 	SS_TOKEN: process.env.SS_TOKEN || '',
 	MAX_UPLOAD: process.env.MAX_UPLOAD || 230,
